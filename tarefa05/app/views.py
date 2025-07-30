5d7f1a0ad2dc1c0c6cae0361bfd4646b975d3928
Williany Vitoria <willianyvitoria2007@gmail.com>
2025-07-30 09:53:14 -0300

    WIP

---
+++ tarefa05/app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from datetime import date
+from .models import Tarefa
+
+def index(request):
+    tarefas = Tarefa.objects.all()
+    context = {
+        'tarefas': tarefas,
+        'hoje': date.today(),
+        #'tarefa_atrasada': , 
+    }
+
+    
+    return render(request, "app/index.html", context)
+
