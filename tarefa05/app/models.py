d22d1f6c475bc4b4c0e62dd49ff3015b50b04094
Williany Vitoria <willianyvitoria2007@gmail.com>
2025-08-01 23:10:56 -0300

    melhorando templates

--- tarefa05/app/models.py
+++ tarefa05/app/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 
+class Autor(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name_plural = "Autores"
+
+
 class Post(models.Model):
     imagem = models.ImageField(upload_to='imagens/')
     titulo = models.CharField(max_length=100)
     texto = models.TextField(max_length=500)
     data = models.DateField()
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.titulo
+    
+    class Meta:
+        verbose_name_plural = "Postagens"
 
 class Blog(models.Model):
     titulo = models.CharField(max_length=100)
@@ -15,3 +29,4 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.titulo
+    
--- tarefa05/app/views.py
+++ tarefa05/app/views.py
@@ -5,15 +5,15 @@ from .models import Post, Blog
 
 def index(request):
     context = {
-        'post': Post.objects.all(),
-        'blog': Blog.objects.all(),
+        'posts': Post.objects.all(),
+        'blog': Blog.objects.first(),
     }
     return render(request, 'app/index.html',context)
 
 def post(request,id_post):
     postagem = get_object_or_404(Post, id=id_post)
     context = {
-        'post': postagem,
-        'blogs': Blog.objects.all(),
+        'posts': postagem,
+        'blog': Blog.objects.first(),
     }
     return render(request, 'app/post.html',context)
